f35e1d279a35d323ea61bdd913afca91551ab3b2
Rafael Torrealba <rafaeltorrealba6@gmail.com>
2018-04-26 15:12:53 -0300

    doing hites

--- app/Http/Controllers/HitesController.php
+++ app/Http/Controllers/HitesController.php
@@ -14,7 +14,22 @@ class HitesController extends Controller
      */
     public function index()
     {
-        //
+        $url = 'https://www.hites.com/tienda/SearchDisplay?categoryId=&storeId=10151&catalogId=10051&langId=-5&sType=SimpleSearch&resultCatEntryType=2&showResultsPage=true&searchSource=Q&pageView=&beginIndex=0&pageSize=-1&searchTerm=smart+tv#facet:-7000000000000005744544839,-10027671&productBeginIndex:0&orderBy:&pageView:grid&minPrice:&maxPrice:&pageSize:&';
+
+        $crawler = Goutte::request('GET', $url);
+        $results = [];
+
+        //TITULO
+        $crawler->filter('.product_name a')->each(function ($node, $index) {
+          dump($node->text());
+        });
+
+        //PRECIO
+        $crawler->filter('.precio_hites')->each(function ($node, $index) {
+          dump($node->text());
+        });
+
+        return view('welcome');
     }
 
     /**
