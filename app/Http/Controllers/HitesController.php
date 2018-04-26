7b92864fd5954ca396a03fcb759d4bed18845889
Rafael Torrealba <rafaeltorrealba6@gmail.com>
2018-04-26 16:57:47 -0300

    hites search

--- app/Http/Controllers/HitesController.php
+++ app/Http/Controllers/HitesController.php
@@ -12,89 +12,75 @@ class HitesController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    
+    protected $products = [];
+    
+    //REVIAR  Symfony\Component\DomCrawler;
+    
     public function index()
     {
+        /*$crawler = Goutte::request('GET', 'https://duckduckgo.com/html/?q=Laravel');
+        $nodes = $crawler->filter('.result__title .result__a')->each(function ($node, $index) {
+
+           $title = $node->text();
+           $this->products[$index]['title'] = $title; 
+
+           return $title;
+
+        });
+
+        $links = $crawler->filter('.result__title .result__a')->each(function ($node, $index) {
+
+            $link = $node->text();
+            $this->products[$index]['link'] = $link;
+
+            return $link;
+
+        });
+        
+        $collection = collect($nodes);
+
+        //$matrix = $collection->combine($links);
+        
+        dd($this->products);*/
+
         $url = 'https://www.hites.com/tienda/SearchDisplay?categoryId=&storeId=10151&catalogId=10051&langId=-5&sType=SimpleSearch&resultCatEntryType=2&showResultsPage=true&searchSource=Q&pageView=&beginIndex=0&pageSize=-1&searchTerm=smart+tv#facet:-7000000000000005744544839,-10027671&productBeginIndex:0&orderBy:&pageView:grid&minPrice:&maxPrice:&pageSize:&';
 
         $crawler = Goutte::request('GET', $url);
-        $results = [];
 
         //TITULO
-        $crawler->filter('.product_name a')->each(function ($node, $index) {
-          dump($node->text());
+        $titles = $crawler->filter('.product_name a')->each(function ($node, $index) {
+           
+           $title = $node->text();
+           $this->products[$index]['title'] = $title;
+
+           return $title;
+
         });
 
         //PRECIO
-        $crawler->filter('.precio_hites')->each(function ($node, $index) {
-          dump($node->text());
+        $prices = $crawler->filter('.precio_hites')->each(function ($node, $index) {
+
+           $price = $node->text();
+           $price = preg_replace("/[^0-9]/", "",$price); //Dejar solo números
+
+           $this->products[$index]['price'] = $price;
+
+           return $price;
+
         });
+
+        dd($this->products);
+
+        $response = [
+            'retail' => 'hites',
+            'findBy' => $url,
+            'products' => $this->products,
+        ];
+
+        dd($response);
 
         return view('welcome');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
