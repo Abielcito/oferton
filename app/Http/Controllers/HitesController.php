4090690633331ede0e33f9c998805404198dbcd5
Rafael Torrealba <rafaeltorrealba6@gmail.com>
2018-04-27 16:30:24 -0300

    dhanges

--- app/Http/Controllers/HitesController.php
+++ app/Http/Controllers/HitesController.php
@@ -19,15 +19,13 @@ class HitesController extends Controller
     
     public function index()
     {
-        $crawler = Goutte::request('GET', 'https://duckduckgo.com/html/?q=Laravel');
+        /*$crawler = Goutte::request('GET', 'https://duckduckgo.com/html/?q=Laravel');
         
         $nodes = $crawler->filter('.links_main')->each(function ($node, $index) {
-               var_dump('.links_main').'<br>';
 
             $title = $node->filter('.result__title .result__a')->each(function ($n, $index) {
 
                $title = $n->text();   
-               var_dump('aqui').'<br>';            
 
                return $title;
 
@@ -37,33 +35,79 @@ class HitesController extends Controller
 
         });
         
-        dd($this->products);
+        dd($this->products);*/
 
-        /*$url = 'https://www.hites.com/tienda/SearchDisplay?categoryId=&storeId=10151&catalogId=10051&langId=-5&sType=SimpleSearch&resultCatEntryType=2&showResultsPage=true&searchSource=Q&pageView=&beginIndex=0&pageSize=-1&searchTerm=smart+tv#facet:-7000000000000005744544839,-10027671&productBeginIndex:0&orderBy:&pageView:grid&minPrice:&maxPrice:&pageSize:&';
+        $url = 'https://www.hites.com/tienda/SearchDisplay?categoryId=&storeId=10151&catalogId=10051&langId=-5&sType=SimpleSearch&resultCatEntryType=2&showResultsPage=true&searchSource=Q&pageView=&beginIndex=0&pageSize=-1&searchTerm=smart+tv#facet:-7000000000000005744544839,-10027671&productBeginIndex:0&orderBy:&pageView:grid&minPrice:&maxPrice:&pageSize:&';
 
         $crawler = Goutte::request('GET', $url);
 
         //TITULO
-        $titles = $crawler->filter('.product_name a')->each(function ($node, $index) {
+
+        $crawler->filter('.grid_mode > li')->each(function ($node, $index) {
            
-           $title = $node->text();
-           $this->products[$index]['title'] = $title;
+            //Buscando link del item
+            
+             $link = $node->filter('.product_name a')->each(function ($n, $index) {
 
-           return $title;
+               $link = $n->extract('href');   
 
+               return $link;
+
+            });
+
+            $this->products[$index]['link'] = $link[0]; 
+
+            
+            //Buscando titulo del item ---------------
+            $title = $node->filter('.product_name a')->each(function ($n, $index) {
+
+               $title = $n->text();   
+
+               return $title;
+
+            });
+
+            $this->products[$index]['title'] = $title[0]; 
+
+
+            //Buscando precio normal del item --------------
+            $price = $node->filter('.precio_normal')->each(function ($n, $index) {
+
+               $price = $n->text();   
+
+               return preg_replace("/[^0-9]/", "",$price);
+
+            });
+
+            if(!isset($price[0])){
+              $price[0] = null;
+            }
+
+            $this->products[$index]['price'] = $price[0]; 
+
+
+            //Buscando precio oferta del item --------------
+            $priceWithCard = $node->filter('.precio_hites')->each(function ($n, $index) {
+
+               $priceWithCard = $n->text();   
+
+               return preg_replace("/[^0-9]/", "", $priceWithCard[0]);
+
+            });
+
+            if(isset($priceWithCard[0])){
+              if($priceWithCard[0] != null && $priceWithCard[0] != ''){
+                 $this->products[$index]['priceWithCard'] = $priceWithCard[0]; 
+              }
+            }else{
+              $priceWithCard[0] = null;
+              $this->products[$index]['priceWithCard'] = $priceWithCard[0]; 
+            }
+
+
+            //Buscando precio oferta del item --------------
         });
 
-        //PRECIO
-        $prices = $crawler->filter('.precio_hites')->each(function ($node, $index) {
-
-           $price = $node->text();
-           $price = preg_replace("/[^0-9]/", "",$price); //Dejar solo números
-
-           $this->products[$index]['price'] = $price;
-
-           return $price;
-
-        });
 
         dd($this->products);
 
@@ -75,7 +119,7 @@ class HitesController extends Controller
 
         dd($response);
 
-        return view('welcome');*/
+        return view('welcome');
     }
 
 }
