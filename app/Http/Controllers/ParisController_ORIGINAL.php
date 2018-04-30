5b9e52246588a67117ca600f3a45c3cbe225de50
daniel garcia <dgarcia@position.cl>
2018-04-30 18:04:39 -0300

    json_filter desde la base de datos para buscar en los items de productos (por ahora solo para retail paris)

--- app/Http/Controllers/ParisController_ORIGINAL.php
+++ app/Http/Controllers/ParisController_ORIGINAL.php
@@ -14,16 +14,14 @@ class ParisController extends MainController {
      * @return \Illuminate\Http\Response
      */
     protected $products = [];
-    protected $filtersCrawler = [];
-    
+
     public function index() {
 
         $store = $this->findStoreByName('paris');
         if (!$store) {
             die('no se encontro el retail');
         }
-        
-        $this->filtersCrawler = json_decode($store->json_filter);
+
         $findByLink = 'https://www.paris.cl/webapp/wcs/stores/servlet/SearchDisplay?searchTermScope=&searchType=1000&filterTerm=&orderBy=2&maxPrice=&showResultsPage=true&langId=-5&beginIndex=0&sType=SimpleSearch&metaData=bWZOYW1lX250a19jczoiSFAi&pageSize=&manufacturer=&resultCatEntryType=&catalogId=40000000629&pageView=image&searchTerm=&facet=ads_f21501_ntk_cs%253A%25228GB%2522&minPrice=&categoryId=51206207&storeId=10801';
 
         $storeCategory = $this->findStoreCategoryByLink($findByLink);
@@ -32,30 +30,30 @@ class ParisController extends MainController {
         }
 
         $crawler = Goutte::request('GET', $findByLink);
-        $node = $crawler->filter($this->filtersCrawler->productBox)->each(function ($node, $index) {
+        $node = $crawler->filter('.boxProduct')->each(function ($node, $index) {
             //PRODUCT NAME 
-            $productName = $node->filter($this->filtersCrawler->productName)->each(function ($n, $j) {
+            $productName = $node->filter('p.sub > a')->each(function ($n, $j) {
                 return $n->text();
             });
             $productName = (isset($productName[0])) ? $productName[0] : null;
 
 
             //PRODUCT LINK
-            $productLink = $node->filter($this->filtersCrawler->productLink)->each(function ($n, $j) {
+            $productLink = $node->filter('p.sub > a')->each(function ($n, $j) {
                 return $n->extract(array('href'));
             });
             $productLink = (isset($productLink[0][0])) ? $productLink[0][0] : null;
 
 
             //PRODUCT PRICE
-            $productPrice = $node->filter($this->filtersCrawler->productPrice)->each(function ($n, $j) {
+            $productPrice = $node->filter('div.itemPrice > p.normal > span')->each(function ($n, $j) {
                 //return preg_replace("/[^0-9]/", "",$n->text());
                 return $this->getJustNumbers($n->text());
             });
             $productPrice = (isset($productPrice[0])) ? $productPrice[0] : null;
 
             //PRODUCT PRICE WITH CARD
-            $productPriceWithCard = $node->filter($this->filtersCrawler->productPriceWithCard)->each(function ($n, $j) {
+            $productPriceWithCard = $node->filter('div.itemPrice > p.price')->each(function ($n, $j) {
                 //return preg_replace("/[^0-9]/", "",$n->text());
                 return $this->getJustNumbers($n->text());
             });
@@ -66,7 +64,7 @@ class ParisController extends MainController {
 
 
             //PRODUCT PRICE INTERNET
-            $productPriceInternet = $node->filter($this->filtersCrawler->productPriceInternet)->each(function ($n, $j) {
+            $productPriceInternet = $node->filter('div.itemPrice > p.internet')->each(function ($n, $j) {
                 return $n->extract(array('data-internet-price'));
             });
             $productPriceInternet = (isset($productPriceInternet[0][0])) ? $productPriceInternet[0][0] : null;
@@ -76,7 +74,7 @@ class ParisController extends MainController {
 
 
             //PRODUCT IMAGE
-            $productImage = $node->filter($this->filtersCrawler->productImage)->each(function ($n, $j) {
+            $productImage = $node->filter('div.item > a.pdp > img')->each(function ($n, $j) {
                 return $n->extract(array('data-src'));
             });
             $productImage = (isset($productImage[0][0])) ? trim($productImage[0][0], '?$plp_moda$') : null;
